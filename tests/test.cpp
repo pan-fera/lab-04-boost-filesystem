b527761f650fd28381b461f94c7a59d5faa20031
pan_fera <pan_fera@mail.ru>
2020-12-14 20:40:36 +0300

    tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -8,7 +8,8 @@ TEST(Example, EmptyTest) {
 
 TEST(Programm, WorkTest) {
     std::cout<<boost::filesystem::current_path()<<std::endl;
-    Filesystem test("../misc/ftp");
+    std::string path("../misc/ftp");
+    Filesystem test(path);
     std::cout<<test;
     EXPECT_TRUE(true);
 }
